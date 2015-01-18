6ae44e20c336a0115e67dd9c83b20a0aadc86e68
Nomura, Yoshihide <nomura@pobox.com>
2015-01-19 05:58:46 +0900

    added tuner.sh. added ignore file for swf and xml.

--- radiko_client.sh
+++ radiko_client.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-#
-# apt-get install mplayer
-# apt-get install rtmpdump swftools libxml2-utils
 
-PORT=8080
-CH=$1
-
-. /home/nomura/radiko_server.sh $CH $PORT
-
-mplayer -ao alsa:device=hw=1.0 -cache 500 http://127.0.0.1:$PORT &
-
-sleep 3600
-
-pkill mplayer
-pkill rtmpgw
+mplayer -ao alsa:device=hw=1.0 -cache 500 http://127.0.0.1:$1 &
 
